d57caa5cc3d1398702aec18c9e165ed972f909c5
bralemant <bralemant@gmail.com>
2026-03-13 13:55:42 -0400

    Add .gitignore for Terraform artifacts

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ resource "azurerm_resource_group" "my_group" {
 }
 
 
+# 3. Creamos las "Tuberías" (Red Virtual)
+resource "azurerm_virtual_network" "mi_red" {
+  name                = "MyVirtualNetwork_CR460"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.mi_grupo.location
+  resource_group_name = azurerm_resource_group.mi_grupo.name
+}
+
+# 4. Creamos una "Habitación" en la red (Subred)
+resource "azurerm_subnet" "my_subnet" {
+  name                 = "MySubNet_CR460"
+  resource_group_name  = azurerm_resource_group.mi_grupo.name
+  virtual_network_name = azurerm_virtual_network.mi_red.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+

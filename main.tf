dad1349a5556a4d0d8c9c4a2a677d9f66d2ff51c
bralemant <bralemant@gmail.com>
2026-03-13 11:20:20 -0400

    Closeup: Resource Group

---
+++ main.tf
@@ -0,0 +1,11 @@
+# 1. Le decimos a Terraform que use Microsoft Azure
+provider "azurerm" {
+  features {}
+}
+
+# 2. Le damos las instrucciones para crear la "Caja" o Grupo de Recursos
+resource "azurerm_resource_group" "my_group" {
+  name     = " MyResourceGroup_CR460"
+  location = "canadacentral"
+}
+

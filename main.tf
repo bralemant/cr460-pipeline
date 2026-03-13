9d6ac78547859acf6aac0a50d603c6aa05020607
bralemant <bralemant@gmail.com>
2026-03-13 15:04:22 -0400

    Update06 Deploying VM eastus size_Standard_B1ms

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_virtual_machine" "my_vm" {
   name                = "MyServer-CR460"
   resource_group_name = azurerm_resource_group.my_group.name
   location            = azurerm_resource_group.my_group.location
-  size                = "Standard_DS1_v2"  # por ejemplo, en lugar de Standard_B1s
+  size                = "Standard_B1ms"  # por ejemplo, en lugar de Standard_B1s
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.my_nic.id,
